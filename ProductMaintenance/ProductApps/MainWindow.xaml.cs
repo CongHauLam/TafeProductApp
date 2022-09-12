f951a45976939c42e1207af8b5c7ed52452c39b4
Cong Hau Lam <cong.lam@student.tafesa.edu.au>
2022-09-12 12:54:56 +0930

    Commit the branch feat_wrap1.1

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25.0m);
+                totalChargeTextBox2.Text = Convert.ToString(cProduct.TotalPayment + 25.0m + 5.0m);
             }
             catch (FormatException)
             {
